d93de3b045cd8d0f37cf3c05d23bc6ee72e4e81b
n10o <0x0082@gmail.com>
2014-04-19 15:28:46 +0900

    Implement error pages

--- index.py
+++ index.py
@@ -13,6 +13,14 @@ def help():
 def userpage(username):
     return 'This is %s\'s page ' % username
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run()
     #app.run(debug=True)
